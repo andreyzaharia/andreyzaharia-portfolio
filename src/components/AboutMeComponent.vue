8df1b3ddfde23af1b6b4a4031394286039fd10f0
andreyzaharia <andreyzaharia122@gmail.com>
2023-09-12 16:54:30 +0200

    contacto

--- src/components/AboutMeComponent.vue
+++ src/components/AboutMeComponent.vue
@@ -1,15 +1,120 @@
 <template>
-  <div>
-    Sobre mi
+  <div class="container">
+      <Card>
+        <template #content>
+          <div class="row">
+            <div class="col-md-4">
+              
+              <img src="/images/andrei.jpg" class="img-fluid rounded" style="max-width: 250px;" />
+            </div>
+            <div class="col-md-8">
+              <p>
+                Soy un apasionado desarrollador junior full stack con una sólida
+                formación y experiencia en el mundo del desarrollo de software.
+                Mi enfoque principal ha sido el desarrollo de aplicaciones
+                móviles Android y en el Backend en Laravel y Java
+              </p>
+
+              <p>
+                Mi experiencia incluye la creación de aplicaciones Android desde
+                cero, lo que implica el diseño de la interfaz de usuario, la
+                implementación de la lógica de negocio y la integración de
+                servicios web y bases de datos. Esto me ha permitido adquirir
+                habilidades sólidas en Java y el ecosistema de desarrollo de
+                Android.
+              </p>
+
+              <p>
+                Además de mi experiencia en el desarrollo móvil, también he
+                trabajado en el desarrollo de backend utilizando tecnologías
+                como Java y Laravel. Esto me ha proporcionado una comprensión
+                profunda de la arquitectura de servidores, la gestión de bases
+                de datos y la creación de API robustas y escalables.
+              </p>
+            </div>
+          </div>
+
+          <hr />
+
+          <div class="d-flex justify-content-start">
+            <h4>Tecnologías</h4>
+          </div>
+
+          <hr />
+
+          <span class="subtitle">Backend</span>
+          <div class="row">
+            <div class="col-md-4">
+              <ul>
+                <li>Java</li>
+                <li>Spring, SpringBoot framework</li>
+                <li>MySQL & NoSQL</li>
+              </ul>
+            </div>
+            <div class="col-md-4">
+              <ul>
+                <li>PHP</li>
+                <li>Laravel framework</li>
+              </ul>
+            </div>
+          </div>
+
+          <span class="subtitle">Frontend</span>
+          <div class="row">
+            <div class="col-md-4">
+              <ul>
+                <li>Laravel framework</li>
+                <li>Javascript</li>
+              </ul>
+            </div>
+            <div class="col-md-4">
+              <ul>
+                <li>VUE 3 framework</li>
+                <li>HTML & CSS</li>
+              </ul>
+            </div>
+          </div>
+
+          <span class="subtitle">Otras tecnologías</span>
+          <div class="row">
+            <div class="col-md-4">
+              <ul>
+                <li>Android studio</li>
+                <li>Control de versiones con Github</li>
+              </ul>
+            </div>
+            <div class="col-md-4">
+              <ul>
+                <li>JIRA</li>
+                <li>FIGMA</li>
+              </ul>
+            </div>
+          </div>
+        </template>
+      </Card>
+      <ContactMeComponent />
+
   </div>
+
 </template>
 
 <script>
-export default {
+import Image from "primevue/image";
+import Card from "primevue/card";
+import ContactMeComponent from "./ContactMeComponent.vue";
 
-}
+export default {
+  components: {
+    Image,
+    Card,
+    ContactMeComponent,
+  },
+};
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+.subtitle {
+  font-size: large;
+  font-weight: 500;
+}
 </style>
---
+++ src/components/ContactMeComponent.vue
@@ -0,0 +1,179 @@
+<template>
+  <Card class="my-4">
+    <template #title>
+      <div class="d-flex justify-content-center">
+        <h4 class="h4">Contactame</h4>
+      </div>
+    </template>
+
+    <template #content>
+      <div class="container">
+        <div class="row">
+          <!-- FORM -->
+          <div class="col-md-6 contenedor1 p-4">
+            <div class="row">
+              <div class="col-md">
+                <label for="email" class="text-primary fw-bold">Email</label>
+                <InputText
+                  id="email"
+                  type="text"
+                  class="w-100"
+                  v-model="email"
+                />
+              </div>
+
+              <div class="col-md">
+                <label for="subject" class="text-primary fw-bold">Asunto</label>
+                <InputText
+                  id="subject"
+                  type="text"
+                  class="w-100"
+                  v-model="subject"
+                />
+              </div>
+            </div>
+
+            <div class="row">
+              <div class="col-md">
+                <label for="message" class="text-primary fw-bold"
+                  >Mensaje</label
+                >
+                <Textarea
+                  id="message"
+                  v-model="message"
+                  rows="5"
+                  cols="30"
+                  class="w-100"
+                />
+              </div>
+            </div>
+
+            <div class="d-flex w-100">
+              <Button
+                type="button"
+                label="Enviar"
+                icon="pi pi-envelope "
+                :loading="loading"
+                @click="load"
+              />
+            </div>
+          </div>
+          <!-- TLF Y EMAIL -->
+
+          <div class="col-md-6 contenedor2">
+     
+            <div class="row justify-content-center h-100 align-content-center">
+
+              <div class="col">
+                <div class="d-flex m-2 align-items-center">
+                <i class="pi pi-linkedin mx-2" style="font-size: 1.5rem; color: white;"></i>
+                <a href="https://www.linkedin.com/in/andrei-zaharia-0279b6249" class="text-white w-100 fs-6">www.linkedin.com/in/andrei-zaharia-0279b6249</a>
+              </div>
+
+              <div class="d-flex m-2 align-items-center">
+                <i class="pi pi-envelope mx-2" style="font-size: 1.5rem; color: white;"></i>
+                <span class="text-white fs-6">andreyzaharia41@gmail.com</span>
+              </div>
+
+              <div class="d-flex m-2 align-items-center">
+                <i class="pi pi-phone mx-2" style="font-size: 1.5rem; color: white;"></i>
+                <span class="text-white fs-6">+34 640 217 315</span>
+              </div>
+              </div>
+              
+            </div>
+              
+    
+              
+
+          </div>
+
+        </div>
+      </div>
+    </template>
+  </Card>
+</template>
+  
+  <script>
+import InputText from "primevue/inputtext";
+import Card from "primevue/card";
+import Textarea from "primevue/textarea";
+import Button from "primevue/button";
+
+export default {
+  components: {
+    InputText,
+    Card,
+    Textarea,
+    Button,
+  },
+
+  data() {
+    return {
+      email: "",
+      subject: "",
+      message: "",
+      loading: false,
+    };
+  },
+
+  methods: {
+    load() {
+      this.loading = true;
+      setTimeout(() => {
+        this.loading = false;
+      }, 2000);
+    },
+  },
+};
+</script>
+  
+<style lang="scss" scoped>
+.rounded {
+  height: 250px;
+  width: 250px;
+  border-radius: 50%;
+  background: green;
+  overflow: hidden;
+}
+
+.subtitle {
+  font-size: large;
+  font-weight: 500;
+}
+
+.contenedor1 {
+  border: 2px;
+  border-style: solid;
+  border-color: $primary;
+  border-radius: 10px 0px 0px 10px;
+
+}
+.contenedor2 {
+  background-color: $primary;
+  border-radius: 0px 10px 10px 0px;
+}
+
+.contact_info {
+  height: 100%;
+  display: grid;
+}
+
+.h4 {
+  color: $primary;
+}
+
+@media only screen and (min-width: $mobile-min-width) and (max-width: $mobile-max-width) {
+  .contenedor1 {
+  border: 2px;
+  border-style: solid;
+  border-color: $primary;
+  border-radius: 10px 10px 0px 0px;
+
+}
+.contenedor2 {
+  background-color: $primary;
+  border-radius: 0px 0px 10px 10px;
+}
+}
+</style>
